3d3a3afdb2288e7ec70ddba966a7f48672457368
LALIE LARABI <lalielarabi@gmail.com>
2026-03-25 16:30:39 +0100

    fin exo2

--- src/exo_2.cpp
+++ src/exo_2.cpp
@@ -7,28 +7,41 @@ bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(),
 
 void merge_sort_merge(std::vector<int> & vec, size_t const left, size_t const middle, size_t const right){
 
-    std::vector<int> vec1(vec[left], vec[middle]);
-    std::vector<int> vec2(vec[middle +1] , vec[right]);
-    for (std::size_t k {0}; k < vec.size(); k++){
-    for (std::size_t i {0}; i < vec1.size(); i++){
-        for (std::size_t j {0}; j < vec2.size(); j++){
-            if(vec1[i]<vec2[j]){
-                vec[k]=vec1[i];
-            }        
-            else{
-                vec[k]=vec2[j];
-                
-            }
+    std::vector<int> vec1(vec.begin() + left, vec.begin() + middle + 1);
+    std::vector<int> vec2(vec.begin() + middle + 1, vec.begin() + right + 1);
+
+    int i{0};
+    int j{0};
+    size_t k{left};
+    while (i<vec1.size() and j<vec2.size()){
+        if (vec1[i]<vec2[j]){
+            vec[k++]=vec1[i];   //on ajoute l'élément de vec1
+            i+=1;
         }
-
+        else{
+            vec[k++]=vec2[j]; //on ajoute l'élément de vec2
+            j+=1;
+        }
     }
-}
-}
+    if (i==vec1.size())
+    {
+        for (int l{j};l<vec2.size();++l){
+            vec[k++]=vec2[l];          //vec1 est vide ont rempli avec le reste de vec2
+        }
+    }
+    if (j==vec2.size())
+    {
+        for (int l{i};l<vec1.size();++l){
+        vec[k++]=vec1[l];     //vec2 est vide ont rempli avec le reste de vec1
+        }
+    }
 
 
+
+}
 
 void merge_sort(std::vector<int> & vec, size_t const left, size_t const right){
-    if (right-left>1){
+    if (right-left>=1){
         merge_sort(vec, left, (right+left)/2);
         merge_sort(vec,((right+left)/2)+1,right);
         merge_sort_merge(vec,left, (right+left)/2,right);     
@@ -41,7 +54,7 @@ void merge_sort(std::vector<int> & vec) {
     merge_sort(vec, 0, vec.size() - 1);
 }
 int main(){
-    std::vector<int> array {1, 52, 5, 15, 60, 7, 27, 6};
+    std::vector<int> array {1, 52, 5, 150, 60, 7, 27, 16};
     merge_sort(array);
 
     if (is_sorted(array)) {
